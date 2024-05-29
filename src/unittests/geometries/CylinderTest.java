6c64074a99547ec30e585803cba68f640d4ec3a6
yoni <yonilev2021@gmail.com>
2024-05-29 11:15:28 +0300

    fix to cylinderTest

--- src/unittests/geometries/CylinderTest.java
+++ src/unittests/geometries/CylinderTest.java
@@ -40,30 +40,22 @@ class CylinderTest {
 		assertEquals(new Vector(0, -1, 0), v1, "getNormal() wrong result");
 		
 		// TC02: Test that getNormal works for the ray base
-	    assertEquals(new Vector(1, 0, 0), v2, "getNormal() not working properly for the ray base");
+	    assertEquals(new Vector(-1, 0, 0), v2, "getNormal() not working properly for the bottom base");
 
 		// TC03: Test that getNormal works for the second base
-		assertEquals(new Vector(-1, 0, 0), v3, "getNormal() not working properly for the second base");
+		assertEquals(new Vector(1, 0, 0), v3, "getNormal() not working properly for the top base");
 
 
 		// =============== Boundary Values Tests ==================
 		
 		Vector v4 = c.getNormal(new Point(4, 0, 0));
 		Vector v5 = c.getNormal(new Point(10, 0, 0));
-		Vector v6 = c.getNormal(new Point(4, 1, 0));
-		Vector v7 = c.getNormal(new Point(10, 1, 0));
 
-		// TC04: Test that getNormal works for the center point of the ray base
-		assertEquals(new Vector(1, 0, 0), v4, "getNormal() not working properly for the center point of the ray base");
+		// TC11: Test that getNormal works for the center point of the ray base
+		assertEquals(new Vector(-1, 0, 0), v4, "getNormal() not working properly for the center point of the bottom base");
 
-		// TC05: Test that getNormal works for the center point of the top base
-		assertEquals(new Vector(-1, 0, 0), v5, "getNormal() not working properly for the center point of the second base");
-		
-		// TC06: Test that getNormal works for the point on the edge of the ray base
-		assertEquals(new Vector(1, 0, 0), v6, "getNormal() not working properly for a point on the edge of the ray base");
-		
-		// TC07: Test that getNormal works for the point on the edge of the second base
-		assertEquals(new Vector(-1, 0, 0), v7, "getNormal() not working properly for a point on the edge of the second base");
+		// TC12: Test that getNormal works for the center point of the top base
+		assertEquals(new Vector(1, 0, 0), v5, "getNormal() not working properly for the center point of the top base");
 
 	}
 
