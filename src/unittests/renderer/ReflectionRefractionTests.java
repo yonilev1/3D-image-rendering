9c118bf4531dbdccdbcf7c9e86ff32c23d0d3dbb
adiel <adiel@localhost>
2024-07-09 09:22:22 +0300

    images for bonus slave 7

--- src/unittests/renderer/ReflectionRefractionTests.java
+++ src/unittests/renderer/ReflectionRefractionTests.java
@@ -92,8 +92,18 @@ public class ReflectionRefractionTests {
 		scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
 		
 		scene.geometries.add( //
-				new Sphere(new Point(-20,-20, -50), 20d).setEmission(new Color(255,190,0))
-				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKT(0.3)),
+				new Sphere(new Point(10,10, -50), 10d).setEmission(new Color(55,90,10))
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKR(0.8)),
+				new Sphere(new Point(0,0, -50), 10d).setEmission(new Color(0,10,100))
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKT(0.6)),
+				new Sphere(new Point(-20,-20, -50), 10d).setEmission(new Color(0,190,0)) 
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKR(0.6)),
+				new Sphere(new Point(-40,-40, -50), 10d).setEmission(new Color(255,190,0))
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKT(0.6)),
+				new Sphere(new Point(-60,-60, -50), 10d).setEmission(new Color(200,0,100))
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKR(0.6)),
+				new Sphere(new Point(-80,-80, -50), 10d).setEmission(new Color(55,90,10))
+				.setMaterial(new Material().setKD(0.001).setKS(0.05).setShininess(10).setKT(0.6)),
 				
 				new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135), new Point(75, 75, -150)) //
 						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(60)), //
@@ -104,14 +114,121 @@ public class ReflectionRefractionTests {
 				new Sphere(new Point(60, 50, -50),30d).setEmission(new Color(BLUE)) //
 						.setMaterial(new Material().setKD(0.2).setKS(0.2).setShininess(30).setKR(0.6)),
 		
-		        new Sphere(new Point(-35, 20, 0),50d).setEmission(new Color(RED)) //
+		        new Sphere(new Point(-35, 20, 10),30d).setEmission(new Color(RED)) //
 		          .setMaterial(new Material().setKD(0.2).setKS(0.2).setShininess(30).setKT(0.7)));
-
-		scene.lights.add(new SpotLight(new Color(400, 700, 400), new Vector(0,0,-1), new Point(0, 0, 50)) //
+	
+		scene.lights.add(new SpotLight(new Color(300, 500, 300), new Vector(0,0,-1), new Point(0, 0, 100)) //
 				.setKL(0.000001).setKQ(0.00001));
 		
 		camera.setLocation(new Point(0, 0, 1000)).setVpDistance(1000).setVpSize(200, 200)
 		.setImageWriter(new ImageWriter("GeometryCombination", 600, 600)).build().renderImage()
 		.writeToImage();
 	}
-}
+	/** Geometry combination including refraction and reflection */
+	@Test
+	public void coloredCubeTest() {
+	    Camera.Builder camera1 = Camera.getBuilder()
+	    		.setDirection(new Vector(-1, -1, -1), new Vector(-1, -1, 2))
+	            .setRayTracer(new SimpleRayTracer(scene));
+	    Camera.Builder camera2 = Camera.getBuilder()
+	    		.setDirection(new Vector(1, 1, -1), new Vector(1, 1, 2))
+	            .setRayTracer(new SimpleRayTracer(scene));
+	    Camera.Builder camera3 = Camera.getBuilder()
+	    		.setDirection(new Vector(0, 0, -1), new Vector(1, 0, 0))
+	            .setRayTracer(new SimpleRayTracer(scene));
+	    Camera.Builder camera4 = Camera.getBuilder()
+	    		.setDirection(new Vector(-1, 0, -1), new Vector( -1, 0,1 ))
+	            .setRayTracer(new SimpleRayTracer(scene));
+
+	    scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.1));
+	   final Material sphereMaterial =new Material().setKD(0.05).setKS(0.25).setShininess(20)
+			                                         .setKR(0.8);
+	   final double faceKT = 0.35;
+	   final double SRadius = 50d;
+	   final Color sphereColor = new Color(BLACK).reduce(2);
+
+	    // Define the eight vertices of the cube
+	    Point v0 = new Point(-50, -50, -50);
+	    Point v1 = new Point(50, -50, -50);
+	    Point v2 = new Point(50, 50, -50);
+	    Point v3 = new Point(-50, 50, -50);
+	    Point v4 = new Point(-50, -50, 50);
+	    Point v5 = new Point(50, -50, 50);
+	    Point v6 = new Point(50, 50, 50);
+	    Point v7 = new Point(-50, 50, 50);
+
+	    scene.geometries.add(
+	            //   Top face (red)
+	            new Polygon(v4, v5, v6, v7).setEmission(new Color(255, 0, 0))
+	                .setMaterial(new Material().setKD(1).setKS(0.5).setKT(faceKT).setShininess(70)),
+
+	            //  Right face (green)
+	            new Polygon(v3, v2, v6, v7).setEmission(new Color(0, 255, 0))
+	                .setMaterial(new Material().setKD(1).setKS(0.5).setKT(faceKT).setShininess(70)),
+
+	            //   Front face (blue)
+	            new Polygon(v1, v5, v6, v2).setEmission(new Color(0, 0, 255))
+	                .setMaterial(new Material().setKD(0.5).setKS(0.5).setKT(faceKT).setShininess(70)),
+
+	            //    Back face (yellow)
+	            new Polygon(v0, v4, v7, v3).setEmission(new Color(255, 255, 0))
+	                .setMaterial(new Material().setKD(0).setKS(0.5).setKT(faceKT).setShininess(70)),
+
+	            //  Bottom face (cyan)
+	            new Polygon(v0, v1, v2, v3).setEmission(new Color(0, 255, 255))
+	                .setMaterial(new Material().setKD(0).setKS(0.5).setKT(faceKT).setShininess(70)),
+
+	            //  Left face (magenta)
+	            new Polygon(v0, v1, v5, v4).setEmission(new Color(255, 0, 255))
+	                .setMaterial(new Material().setKD(0).setKS(0.5).setKT(faceKT).setShininess(70)),
+	                
+	                new Plane(new Point(0, 0, -300), new Vector(0, 0, 1)).setMaterial(new Material().setKD(0.9).setKR(0))
+					.setEmission(new Color(blue).reduce(4)),
+	                
+	                new Sphere(new Point(0, 0, 0), 30d).setEmission(new Color(200, 200, 200)).
+	                setMaterial(new Material().setKD(0.25).setKS(0.25).setShininess(20)),
+	                
+	                new Sphere(new Point(0, 0, 180), SRadius).setEmission(sphereColor).
+	                setMaterial(sphereMaterial),
+	                
+	                new Sphere(new Point(0, 170, 0), SRadius).setEmission(sphereColor).
+	                setMaterial(sphereMaterial),
+	                
+	                new Sphere(new Point(170, 0, 0), SRadius).setEmission(sphereColor).
+	                setMaterial(sphereMaterial),
+	                
+	                new Sphere(new Point(0, 0, -170), SRadius).setEmission(sphereColor).
+	                setMaterial(sphereMaterial),
+	                
+	                new Sphere(new Point(0, -170, 0), SRadius).setEmission(sphereColor).
+	                setMaterial(sphereMaterial),
+	                
+	                new Sphere(new Point(-170, 0, 0), SRadius).setEmission(sphereColor.add()).
+	                setMaterial(sphereMaterial)
+	                
+	    );
+
+	  scene.lights.add(new PointLight(new Color(250, 250, 250), new Point(200, 200, 200))
+	            .setKL(0.00001).setKQ(0.00001));
+	    
+	  scene.lights.add(new PointLight(new Color(155, 155, 155), new Point(-200, -200, -200))
+	          .setKL(0.0001).setKQ(0.00001));
+
+	    camera1.setLocation(new Point(500, 500, 500)).setVpDistance(300).setVpSize(200,200)
+	        .setImageWriter(new ImageWriter("ColoredCube", 600,600)).build().renderImage()
+	        .writeToImage();
+	    camera2.setLocation(new Point(-500, -500, 500)).setVpDistance(200).setVpSize(200,200)
+        .setImageWriter(new ImageWriter("ColoredCubeBACK", 600,600)).build().renderImage()
+        .writeToImage();
+	    camera3.setLocation(new Point(0, 0, 500)).setVpDistance(100).setVpSize(200,200)
+        .setImageWriter(new ImageWriter("ColoredCubeDOWN", 600,600)).build().renderImage()
+        .writeToImage();
+	    camera4.setLocation(new Point(350, 0, 400)).setVpDistance(300).setVpSize(200,200)
+        .setImageWriter(new ImageWriter("ColoredCubeFRONT", 600,600)).build().renderImage()
+        .writeToImage();
+	}
+
+
+	} 
+	
+	 
